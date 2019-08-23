4d0cc0f75ca5db97d6f1a964b51e3504eb8d2f6d
Timothy Shores <timothymshores@gmail.com>
2019-08-22 21:09:03 -0500

    Added ability to click a number and it displays in the calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
@@ -7,12 +7,8 @@ import Logo from "./components/DisplayComponents/Logo";
 
 import "./App.css";
 
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
-
 function App() {
+	const [currentNum, setCurrentNum] = useState();
 	// STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
 	// Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
 	// Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -23,11 +19,11 @@ function App() {
 		<div className="container">
 			<div className="calculator">
 				<Logo />
-				<Display />
+				<Display number={currentNum} />
 				<div className="buttons">
 					<div className="buttons-left">
 						<Specials />
-						<Numbers />
+						<Numbers setNum={number => setCurrentNum(number)} />
 					</div>
 					<div className="buttons-right">
 						<Operators />
